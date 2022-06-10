1f20bbc1bad7fa6dd516f70ab2032b16367167fa
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-06-09 18:08:04 -0600

    Add error handling and file structure for testing

--- cypress/e2e/homepage-spec.cy.js
+++ cypress/e2e/homepage-spec.cy.js
@@ -1,11 +1,15 @@
-// describe("homepage", () => {
-//   beforeEach(() => {
-//       cy.visit("http://localhost:3000/")
-//       cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/search?&hasImages=true&q=Paintings&isHighlight=true", {"objectIDs":  ["764095", "764091", "768547"]})
-//   })
+describe("homepage", () => {
+  beforeEach(() => {
+      cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/search?&hasImages=true&q=Paintings&isHighlight=true", {fixture : "ids.json"})
+      cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/764095", {fixture: "work1.json"} )
+      cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/764091", {fixture: "work2.json"} )
+      cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/768547", {fixture: "work3.json"} )
+      cy.visit("http://localhost:3000/")
+      cy.wait(2000)
+  })
 
-//   it("should show the user a homepage with a header, nav bar and several thumnails", () => {
-//       cy.contains("Art Access")
-//       cy.get(".works-container").children().should("have.length", 3)
-//   })
-// })
+  it("should show the user a homepage with a header, nav bar and several thumnails", () => {
+      cy.contains("Art Access")
+      cy.get(".works-container").children().should("have.length", 2)
+  })
+})
