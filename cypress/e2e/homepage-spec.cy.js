618f687356d8b81d4f73506fabb8f425ea046f6b
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-06-10 14:24:36 -0600

    Correct stubbing

--- cypress/e2e/homepage-spec.cy.js
+++ cypress/e2e/homepage-spec.cy.js
@@ -1,15 +1,18 @@
 describe("homepage", () => {
   beforeEach(() => {
-      cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/search?&hasImages=true&q=Paintings&isHighlight=true", {fixture : "ids.json"})
-      cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/764095", {fixture: "work1.json"} )
-      cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/764091", {fixture: "work2.json"} )
-      cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/768547", {fixture: "work3.json"} )
-      cy.visit("http://localhost:3000/")
-      cy.wait(2000)
+      cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/search?&hasImages=true&q=Paintings&isHighlight=true", {fixture : "ids"})
+
+     
+
+      cy.visit("http://localhost:3000/").wait(2000)      
   })
 
   it("should show the user a homepage with a header, nav bar and several thumnails", () => {
       cy.contains("Art Access")
-      cy.get(".works-container").children().should("have.length", 2)
+      cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/764095", {fixture: "work1"} )
+
+      cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/764091", {fixture: "work2"} )
+      cy.intercept("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/768547", {fixture: "work3"} )
+      cy.get(".works-container").children().should("have.length", 3)
   })
 })
